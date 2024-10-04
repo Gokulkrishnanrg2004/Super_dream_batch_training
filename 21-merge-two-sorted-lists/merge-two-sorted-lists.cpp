0d783e9b4a02410bf780dbbbe465cf21802f7384
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-04 09:43:09 +0530

    Time: 4 ms (68.89%) | Memory: 19.7 MB (57.11%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,24 +1,58 @@
+// class Solution {
+// public:
+// 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
+//     {
+//         if(l1==NULL)
+//         {
+//             return l2;
+//         }	
+//         if(l2==NULL)
+//         {
+//             return l1;
+//         }
+//         if(l1->val <= l2->val)
+//         {
+//             l1->next = mergeTwoLists(l1->next,l2);
+//             return l1;
+//         }
+//         else 
+//         {
+//             l2->next = mergeTwoLists(l1,l2->next);
+//             return l2;
+//         }
+// 	}
+// };	
 class Solution {
 public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
     {
-        if(l1==NULL)
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* current = dummyNode;
+        ListNode* temp1 = l1;
+        ListNode* temp2 = l2;
+        while(temp1!=nullptr && temp2!=nullptr)
         {
-            return l2;
-        }	
-        if(l2==NULL)
-        {
-            return l1;
+            if(temp1->val >= temp2->val)
+            {
+                current->next = temp2;
+                temp2 = temp2->next;
+                current = current->next;
+            }
+            else 
+            {
+                current->next = temp1;
+                temp1 = temp1->next;
+                current = current->next;
+            }
         }
-        if(l1->val <= l2->val)
-        {
-            l1->next = mergeTwoLists(l1->next,l2);
-            return l1;
-        }
-        else 
-        {
-            l2->next = mergeTwoLists(l1,l2->next);
-            return l2;
-        }
+            if(temp1!=nullptr)
+            {
+                current->next = temp1;
+            }
+            else if(temp2!=nullptr)
+            {
+                current->next = temp2;
+            }
+            return dummyNode->next;
 	}
 };	
