5ad3eef5b70d6a5a265f826f03b0be07233ffae4
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-09 11:59:26 +0530

    Time: 54 ms (94.00%) | Memory: 45.4 MB (45.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,35 +2,35 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        // unordered_map<int,int>Map;
-        // Map[0] = 1;
-        // int sum=0,count=0;
-        // int n = nums.size();
-        // for(int i=0;i<n;i++)
-        // {
-        //     sum+=nums[i];
-        //     int rem = sum - k;
-        //     if(Map.find(rem)!=Map.end())
-        //     {
-        //         count += Map[rem];
-        //     }
-        //     Map[sum]++;
-        // }
-        // return count;
-        int n = nums.size();
+        unordered_map<int,int>Map;
+        Map[0] = 1;
         int sum=0,count=0;
+        int n = nums.size();
         for(int i=0;i<n;i++)
         {
-            sum=0;
-            for(int j=i;j<n;j++)
+            sum+=nums[i];
+            int rem = sum - k;
+            if(Map.find(rem)!=Map.end())
             {
-                sum+=nums[j];
-                if(sum==k)
-                {
-                    count++;
-                }
+                count += Map[rem];
             }
+            Map[sum]++;
         }
         return count;
+        // int n = nums.size();
+        // int sum=0,count=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     sum=0;
+        //     for(int j=i;j<n;j++)
+        //     {
+        //         sum+=nums[j];
+        //         if(sum==k)
+        //         {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 };
