6e0b5574773713e20056017316745bdc4cb86283
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-15 14:28:27 +0530

    Time: 22 ms (86.66%) | Memory: 42 MB (6.98%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,19 +4,19 @@ public:
     {
         int n = nums.size();
         vector<int>ans;
-        vector<int>a(n,1);
+        vector<int>a(n,1); // Inittialize the array a with 1's
         for(int i=1;i<n;i++)
         {
-            a[i] = a[i-1]*nums[i-1];
+            a[i] = a[i-1]*nums[i-1]; // 1 1 2 6
         }    
         vector<int>b(n,1);
         for(int i=n-2;i>=0;i--)
         {
-            b[i] = b[i+1]*nums[i+1];
+            b[i] = b[i+1]*nums[i+1]; // 24 12 4 1
         }
         for(int i=0;i<n;i++)
         {
-            int prod = a[i]*b[i];
+            int prod = a[i]*b[i]; // 24 12 8 6
             ans.push_back(prod);
         }
         return ans;
