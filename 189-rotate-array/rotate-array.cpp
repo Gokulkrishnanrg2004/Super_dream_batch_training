378fc9a72e083b318862bfc5131a1c47b62079ec
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-15 13:59:05 +0530

    Time: 16 ms (86.22%) | Memory: 27.7 MB (50.25%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -27,8 +27,9 @@ public:
         // }
         int n = nums.size();
         k = k % n;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
+        // [1,2,3,4,5,6,7] k = 3
+        reverse(nums,0,n-1); // 0 to n-1 it reverse the whole [7,6,5,4,3,2,1]
+        reverse(nums,0,k-1); // 0 to k-1 it reverse before the 3 index element [5,6,7,4,3,2,1] 
+        reverse(nums,k,n-1); // k to n-1 3rd index to last index [5,6,7,1,2,3,4]
     }
 };
