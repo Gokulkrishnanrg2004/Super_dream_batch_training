dccc34628a7919a4d3dd0a9901024804af159767
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-10-15 16:06:14 +0530

    Time: 2 ms (91.67%) | Memory: 12.2 MB (59.13%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) 
     {
+        //Using ones and twos approach based on the occurence
         int ones = 0;
         int twos = 0;
         for(int i=0;i<nums.size();i++)
