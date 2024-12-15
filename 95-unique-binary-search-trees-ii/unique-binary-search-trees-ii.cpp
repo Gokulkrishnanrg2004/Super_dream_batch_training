60cba8925db1b19068ac42ae873b665df5dd451d
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-12-15 17:53:19 +0530

    Time: 4 ms (44.78%) | Memory: 19.1 MB (58.92%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -11,38 +11,36 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> generateTrees(int start,int end) 
+    vector<TreeNode*> generateTrees(int start,int end)
     {
-            vector<TreeNode*> result;
+        vector<TreeNode*>result;
+        if(start > end)
+        {
+            result.push_back(nullptr);
+            return result;
+        }
+        for(int i=start;i<=end;i++)
+        {
+            vector<TreeNode*>leftSub = generateTrees(start,i-1);
+            vector<TreeNode*>rightSub = generateTrees(i+1,end);
 
-    // Base case: if start > end, return null
-    if (start > end) {
-        result.push_back(nullptr);
-        return result;
-    }
-
-    // Loop through all values as root
-    for (int i = start; i <= end; i++) {
-        // Generate all left subtrees with values [start, i-1]
-        vector<TreeNode*> leftSubtrees = generateTrees(start, i - 1);
-        // Generate all right subtrees with values [i+1, end]
-        vector<TreeNode*> rightSubtrees = generateTrees(i + 1, end);
-
-        // Combine left and right subtrees with the current root
-        for (auto left : leftSubtrees) {
-            for (auto right : rightSubtrees) {
-                TreeNode* root = new TreeNode(i);
-                root->left = left;
-                root->right = right;
-                result.push_back(root);
+            for(auto left : leftSub)
+            {
+                for(auto right : rightSub)
+                {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = left;
+                    root->right = right;
+                    result.push_back(root);
+                }
             }
         }
+        return result;
     }
-
-    return result;    
+    vector<TreeNode*> generateTrees(int n) 
+    {
+        if(n==0)
+            return {};
+        return generateTrees(1,n);    
     }
-    vector<TreeNode*> generateTrees(int n) {
-    if (n == 0) return {};
-    return generateTrees(1, n);
-}
 };
