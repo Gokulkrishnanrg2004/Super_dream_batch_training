11df5bdae3633bf6f47aac8814a1a2df4d2fba86
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-12-25 07:28:45 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (8.77%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    void reverse(vector<int>& arr,int l,int r)
-    {
-        while(l<=r)
-        {
-            swap(arr[l],arr[r]);
-            l++;
-            r--;
-        }
-    }
+    // void reverse(vector<int>& arr,int l,int r)
+    // {
+    //     while(l<=r)
+    //     {
+    //         swap(arr[l],arr[r]);
+    //         l++;
+    //         r--;
+    //     }
+    // }
     void rotate(vector<int>& nums, int k) 
     {
-        // vector<int>ans;
-        // int n = nums.size();
-        // for(int i=n-k;i<n;i++)
-        // {
-        //     ans.push_back(nums[i]);
-        // }
-        // for(int i=0;i<n-k;i++)
-        // {
-        //     ans.push_back(nums[i]);
-        // }
-        // for(int i=0;i<ans.size();i++)
-        // {
-        //     nums[i] = ans[i];
-        // }
+        vector<int>ans;
         int n = nums.size();
-        k = k % n;
-        // [1,2,3,4,5,6,7] k = 3
-        reverse(nums,0,n-1); // 0 to n-1 it reverse the whole [7,6,5,4,3,2,1]
-        reverse(nums,0,k-1); // 0 to k-1 it reverse before the 3 index element [5,6,7,4,3,2,1] 
-        reverse(nums,k,n-1); // k to n-1 3rd index to last index [5,6,7,1,2,3,4]
+        k = k%n;
+        if(k==0 || n==0)
+        {
+            return;
+        }
+        for(int i=n-k;i<n;i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<n-k;i++)
+        {
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<ans.size();i++)
+        {
+            nums[i] = ans[i];
+        }
+        // int n = nums.size();
+        // k = k % n;
+        // // [1,2,3,4,5,6,7] k = 3
+        // reverse(nums,0,n-1); // 0 to n-1 it reverse the whole [7,6,5,4,3,2,1]
+        // reverse(nums,0,k-1); // 0 to k-1 it reverse before the 3 index element [5,6,7,4,3,2,1] 
+        // reverse(nums,k,n-1); // k to n-1 3rd index to last index [5,6,7,1,2,3,4]
     }
 };
