ee6e50623d249e0d0ffc81a3e24f7458c80f66e7
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2024-12-28 17:54:54 +0530

    Time: 4 ms (49.73%) | Memory: 9.9 MB (32.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,24 +4,39 @@ public:
     {
         if(s.size() != t.size()) return false;
 
-        unordered_map<char,int>Map1;    
-        unordered_map<char,int>Map2;    
+        //Method 1
+        // unordered_map<char,int>Map1;    
+        // unordered_map<char,int>Map2;    
 
-        for(char c : s)
-        {
-            Map1[c]++;
-        }
-        for(char c : t)
-        {
-            Map2[c]++;
-        }
+        // for(char c : s)
+        // {
+        //     Map1[c]++;
+        // }
+        // for(char c : t)
+        // {
+        //     Map2[c]++;
+        // }
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     char c = s[i];
+        //     if(Map1[c] != Map2[c])
+        //     {
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        //Method 2
+        unordered_map<char,int>Map;
         for(int i=0;i<s.size();i++)
         {
-            char c = s[i];
-            if(Map1[c] != Map2[c])
-            {
+            Map[s[i]]++;
+            Map[t[i]]--;
+        }
+        for(auto it : Map)
+        {
+            if(it.second != 0)
                 return false;
-            }
         }
         return true;
     }
