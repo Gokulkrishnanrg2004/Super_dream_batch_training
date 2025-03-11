fc14d5bea7b8ef3cd6e9caa5c1a842aff3f40f5c
Gokulkrishnanrg2004 <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2025-03-11 09:47:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (70.55%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) 
     {
-        vector<vector<int>>result;
-        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end()); 
         do 
         {
-            vector<int>arr;
-            for(int num : nums)
-            {
-                arr.push_back(num);
-            }
-            result.push_back(arr);
-        }while(next_permutation(nums.begin(),nums.end()));
-        return result; 
+            result.push_back(nums);
+            
+        } while (next_permutation(nums.begin(), nums.end()));
+        return result;
     }
 };
