7b5cad29383d7f1d0119ec4d04682d030ea0015d
Gokul Krishnan R G <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2025-07-01 14:24:56 +0530

    Time: 9 ms (8.02%) | Memory: 8.8 MB (67.65%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrangeCoins(int n) 
+    {
+        for(long i=2;;i++)
+        {
+            long sum = (i*(i+1))/2;
+            if(sum > n) return i-1;
+            if(n == sum) return i;
+        }
+        return -1;
+    }
+};
