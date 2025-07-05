b6ad158a2c8095c2baf357faed0bb3b912a994c6
Gokul Krishnan R G <125328166+Gokulkrishnanrg2004@users.noreply.github.com>
2025-07-05 11:13:47 +0530

    Time: 2 ms (19.49%) | Memory: 46.3 MB (46.99%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -6,18 +6,17 @@ public:
         int low = 0, high = n - 1;
         int currentSum = 0;
 
-        // Step 1: Take the first k cards from the start (low)
-        for (int i = 0; i < k; i++) {
+        for (int i = 0; i < k; i++) 
+        {
             currentSum += cardPoints[i];
         }
 
         int maxSum = currentSum;
-
-        // Step 2: Gradually replace cards from the front (low) with cards from the end (high)
-        for (int i = 0; i < k; i++) {
-            currentSum -= cardPoints[k - i - 1]; // Remove the last card from the front
-            currentSum += cardPoints[high - i];  // Add the card from the end
-            maxSum = max(maxSum, currentSum);    // Track the maximum sum
+        for (int i = 0; i < k; i++) 
+        {
+            currentSum -= cardPoints[k - i - 1]; 
+            currentSum += cardPoints[high - i]; 
+            maxSum = max(maxSum, currentSum);   
         }
 
         return maxSum;
